2a1dff847f299918d6395da3c62fafc7af55a0d7
petermordaunt <peetfire@hotmail.com>
2022-01-09 23:33:39 +0000

    Styling - added background img and img for categories display. Started to style add rating form

--- js/app.js
+++ js/app.js
@@ -23,37 +23,36 @@ const handleRangeChange = function(){
 }
 
 const createMovieListItem = function(form) {
-    const movieListItem = document.createElement('li');
-    movieListItem.classList.add('movie-list-item');
+    const movieListItem = document.createElement('div');
+    if(form.category.value === "movie"){
+        movieListItem.classList.add('movie-list-item');
 
-    const category = document.createElement('h2');
-    category.textContent = form.category.value;
-    movieListItem.appendChild(category);
+        const movieItemContainer = document.createElement('div');
+        movieItemContainer.classList.add('movie-item-container');
 
-    const title = document.createElement('h3');
-    title.textContent = form.title.value;
-    movieListItem.appendChild(title);
+        const line1 = createLine1(form);
+        const line2 = createLine2(form);
+        const scoreBar = createScoreBar(form.score.value);
+        movieItemContainer.append(line1);
+        movieItemContainer.append(line2);
+        movieItemContainer.append(scoreBar);
 
-    const genre = document.createElement('h4');
-    genre.textContent = form.genre.value;
-    movieListItem.appendChild(genre);
+        movieListItem.append(movieItemContainer);
+    } else {
+        movieListItem.classList.add('tv-list-item');
 
-    const ratings = document.querySelectorAll('#rating');
-    const rating = getAgeRating(ratings);
-    const ageRating = document.createElement('h5');
-    ageRating.textContent = rating.value;
-    movieListItem.appendChild(ageRating);
+        const tvItemContainer = document.createElement('div');
+        tvItemContainer.classList.add('tv-item-container');
 
-    const contains = document.querySelectorAll('#contains');
-    const containsValues = getContainsValues(contains);
-    const contentWarning = document.createElement('h5');
-    contentWarning.textContent = containsValues;
-    movieListItem.appendChild(contentWarning);
+        const line1 = createLine1(form);
+        const line2 = createLine2(form);
+        const scoreBar = createScoreBar(form.score.value);
+        tvItemContainer.append(line1);
+        tvItemContainer.append(line2);
+        tvItemContainer.append(scoreBar);
 
-    const score = document.createElement('p');
-    const scoreBar = createScoreBar(form.score.value);
-    score.textContent = form.score.value;
-    movieListItem.appendChild(scoreBar);
+        movieListItem.append(tvItemContainer);
+    }
 
     return movieListItem;
 }
@@ -74,26 +73,66 @@ const getContainsValues = function(contains){
     }
     return values;
 }
+const createLine1 = function(form){
+    // create line1 container
+    const line1 = document.createElement('div');
+    line1.classList.add('line1-container');
+
+    // create content for line1
+    const title = document.createElement('h3');
+    title.textContent = form.title.value;
+
+    // apend content to line1
+    line1.append(title);
+
+    return line1;
+}
+const createLine2 = function(form){
+    // create line2 container
+    const line2 = document.createElement('div');
+    line2.classList.add('line2-container');
+
+    // create content for line 2
+    const genre = document.createElement('h4');
+    genre.textContent = form.genre.value;
+    
+    const contains = document.querySelectorAll('#contains');
+    const containsValues = getContainsValues(contains);
+    const contentWarning = document.createElement('h5');
+    contentWarning.textContent = containsValues;
+    
+    const ratings = document.querySelectorAll('#rating');
+    const rating = getAgeRating(ratings);
+    const ageRating = document.createElement('h5');
+    ageRating.textContent = rating.value;
+
+    // append content to line2
+    line2.append(genre);
+    line2.append(contentWarning);
+    line2.append(ageRating);
+
+    return line2;
+}
 const createScoreBar = function(score){
-    const scoreWrapper = document.createElement('div');
-    scoreWrapper.classList.add('score-wrapper');
+    const scoreContainer = document.createElement('div');
+    scoreContainer.classList.add('score-container');
     const scoreValue = document.createElement('p');
     scoreValue.textContent = String(score) + "%";
     scoreValue.classList.add('score-text');
-    scoreWrapper.appendChild(scoreValue);
+    scoreContainer.appendChild(scoreValue);
 
     
     for(let i=0; i <= 100 ; i++){
         if(i<=score){
             const scoreFill = document.createElement('div');
             scoreFill.classList.add('score-fill');
-            scoreWrapper.append(scoreFill);
+            scoreContainer.append(scoreFill);
         }
         else{
             const scoreNoFill = document.createElement('div');
             scoreNoFill.classList.add('score-no-fill');
-            scoreWrapper.append(scoreNoFill);
+            scoreContainer.append(scoreNoFill);
         }
     }
-    return scoreWrapper;
+    return scoreContainer;
 }
